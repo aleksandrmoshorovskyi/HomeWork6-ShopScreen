06e31d328506e4374353c1d91dc5bb789923c3f7
Oleksandr Moshorovskyi <46052383+aleksandrmoshorovskyi@users.noreply.github.com>
2024-03-12 12:30:44 +0200

    UI

--- HomeWork6-ShopScreen/Source/ViewController/ViewController.swift
+++ HomeWork6-ShopScreen/Source/ViewController/ViewController.swift
@@ -18,6 +18,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var buyButton: UIButton!
     @IBOutlet weak var buy2Button: UIButton!
     
+    @IBAction func ckickedBuyBtn(_ sender: UIButton) {
+        print("ckickedBuyBtn")
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func clickedBuy2Btn(_ sender: UIButton) {
+        print("clickedBuy2Btn")
+        self.present(alert2, animated: true, completion: nil)
+    }
+    
     var goods1 = Goods(name: "Материнська плата Asus rog strix b560-e Gaming wifi (s1200 Intel b560 ddr4)",
                        code: "370030773",
                        price: 9531.00,
@@ -28,17 +38,51 @@ class ViewController: UIViewController {
                                 "motherboard_pic4",
                                 "motherboard_pic5",])
     
+    let alert = UIAlertController(title: "Купити в кредит", message: "Цією дією Ви погоджуєтесь з умовами договору!", preferredStyle: .alert)
+    let aletalertAction = UIAlertAction(title: "OK", style: .default, handler: { _ in NSLog("The \"OK\" alert occured.")})
+    
+    let alert2 = UIAlertController(title: "Купити зараз", message: "Цією дією Ви погоджуєтесь з умовами договору!", preferredStyle: .alert)
+    let aletalertAction2 = UIAlertAction(title: "OK", style: .default, handler: { _ in NSLog("The \"OK\" alert occured.")})
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.dataSource = self
         collectionView.delegate = self
         
+        // create attributed string
+        let lableStr1 = "Самовивіз з наших магазинів - "
+        let str1Attribute = [ NSAttributedString.Key.foregroundColor: UIColor.black ]
+        let lableStr1Attributed = NSAttributedString(string: lableStr1, attributes: str1Attribute)
+        
+        let lableStr2 = "БЕЗКОШТОВНО"
+        let str2Attribute = [ NSAttributedString.Key.foregroundColor: UIColor.systemGreen ]
+        let lableStr2Attributed = NSAttributedString(string: lableStr2, attributes: str2Attribute)
+        
+        var attributedString = NSMutableAttributedString(string: "")
+        attributedString.append(lableStr1Attributed)
+        attributedString.append(lableStr2Attributed)
+        
+
+        // set attributed text on a UILabel
+        //myLabel.attributedText = myAttrString
+        
         titleLable.text = goods1.name
         codeLabel.text = "Код \(goods1.code)"
         priceLable.text = "\(goods1.price)"
-        textLable.text = "Самовивіз з наших магазинів"
+        //textLable.text = "Самовивіз з наших магазинів"
+        textLable.attributedText = attributedString
         text2Lable.text = "Забрати завтра з 12:00"
+        
+        buyButton.setTitle("Купити в кредит", for: .normal)
+        buyButton.setTitle("Купити в кредит", for: .selected)
+        
+        buy2Button.setTitle("Купити зараз", for: .normal)
+        buy2Button.setTitle("Купити зараз", for: .selected)
+        
+        alert.addAction(aletalertAction)
+        alert2.addAction(aletalertAction2)
     }
         
 }
